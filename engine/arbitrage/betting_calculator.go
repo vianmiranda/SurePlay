204b8a7a1343733280b22311c75bf3006346b35f
Yash Sakharkar <yash.sakharkar0327@gmail.com>
2023-12-30 23:23:44 -0500

    added calculator functionality

---
+++ engine/arbitrage/betting_calculator.go
@@ -0,0 +1,26 @@
+package arbitrage
+
+import (
+	"gonum.org/v1/gonum/mat"
+)
+
+// given a amount to bet on a line, returns amount to bet on other line to ensure a profit
+func ensure_profit(odds1 float32, amt float32, odds2 float32) float32 {
+	return (odds1 * amt) / odds2
+}
+
+// given a total budget the user wants to bet, returns the ideal way to split up the bet
+func split_budget(odds1 float32, odds2 float32, budget float32) (float64, float64) {
+
+	A := mat.NewDense(2, 2, []float64{1, 1, float64(odds1), float64(-odds2)})
+	b := mat.NewVecDense(2, []float64{float64(budget), 0})
+
+	// Solve the equations using the Solve function.
+	var x mat.VecDense
+	err := x.SolveVec(A, b)
+	if err != nil {
+		return 0, 0
+	}
+
+	return x.At(0, 0), x.At(1, 0)
+}
--- engine/go.mod
+++ engine/go.mod
@@ -1,3 +1,5 @@
 module engine
 
 go 1.21.5
+
+require gonum.org/v1/gonum v0.14.0 // indirect
